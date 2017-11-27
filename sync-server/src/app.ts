1978726e742f44509ee51a2ef0b97cee33ea59d7
Jan S <jan.schmitter@gmail.com>
2017-11-27 20:46:47 +0100

    Minor cleanup

--- sync-server/src/app.ts
+++ sync-server/src/app.ts
@@ -4,17 +4,16 @@ import { ProjectData } from './database/ProjectData';
 import { Database } from './Database';
 import { Server } from './server';
 
-export var database:Database = new Database();
-export var server:Server = null;
+export const database:Database = new Database();
+export const server:Server = new Server();
 
-export var projectsManager:ProjectsManager = new ProjectsManager();
-export var syncManager:SyncManager = new SyncManager();
+export const projectsManager:ProjectsManager = new ProjectsManager();
+export const syncManager:SyncManager = new SyncManager();
 
 database.initialize()
 .then(() => {
     console.log("[Database] Connected!");
 
-    server = new Server();
     server.startServer();
 })
 .catch(() => {
